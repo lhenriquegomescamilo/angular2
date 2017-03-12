ec15e86ec321d8f0278219c004cf4022151287fb
Luis H. Camilo <lhenriquegomescamilo@hotmail.com>
2017-03-12 18:18:49 -0300

    master: Melhorando o uso de diretivas

--- creating-new-directive/src/app/shared/background-yellow.directive.ts
+++ creating-new-directive/src/app/shared/background-yellow.directive.ts
@@ -1,13 +1,19 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
   selector: 'p[backgroundYellow]'
 })
 export class BackgroundYellowDirective {
 
-  constructor(private _elementRef: ElementRef) {
-    this._elementRef.nativeElement.style.backgroundColor = 'yellow';
-    console.log("Só pra avisar, ainda estou aqui veio")
+  constructor(private _elementRef: ElementRef, private _renderer: Renderer) {
+    //this._elementRef.nativeElement.style.backgroundColor = 'yellow';
+    //console.log("Só pra avisar, ainda estou aqui veio")
+
+    this._renderer.setElementStyle(
+      this._elementRef.nativeElement,
+      'background-color',
+      'yellow'
+    );
   }
 
 }
