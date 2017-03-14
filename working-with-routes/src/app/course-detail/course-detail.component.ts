98bf7f58f7743947a7433cf825c18c2175a0716c
Luis H. Camilo <lhenriquegomescamilo@hotmail.com>
2017-03-14 00:45:03 -0300

    master: Criando uma navegação com o router

--- working-with-routes/src/app/course-detail/course-detail.component.ts
+++ working-with-routes/src/app/course-detail/course-detail.component.ts
@@ -1,18 +1,19 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Subscription} from "rxjs/Rx";
-
+import {CourseService} from "../course/course.service";
+import * as _ from 'lodash';
 @Component({
   selector: 'app-course-detail',
   templateUrl: 'course-detail.component.html',
   styleUrls: ['course-detail.component.css']
 })
 export class CourseDetailComponent implements OnInit,  OnDestroy {
-
   id: string = '';
-  subscription: Subscription;
 
-  constructor(private router: ActivatedRoute) {
+  subscription: Subscription;
+  private _course: any;
+  constructor(private router: ActivatedRoute, private _courseService: CourseService, private _router: Router) {
     //  this.id = this.router.snapshot.params['id'];
   }
 
@@ -21,11 +22,20 @@ export class CourseDetailComponent implements OnInit,  OnDestroy {
       .params
       .subscribe((params: any) => {
         this.id = params['id'];
+        this._course = this._courseService.findCourseById(_.toNumber(this.id));
+        if(_.isNull(this._course)){
+          // Redirecionar para alguma tela de not-found
+            this._router.navigate(['/login'])
+        }
       });
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  get course(): any {
+    return this._course;
   }
 
 }
--- working-with-routes/src/app/course/course.service.ts
+++ working-with-routes/src/app/course/course.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
+import * as _ from 'lodash';
 
 @Injectable()
 export class CourseService {
 
+  private _courses: any[] = [
+    {id: 1, name : 'Angular2'},
+    {id: 2, name : 'Java'},
+  ];
   constructor() {
     console.log('Só pra constar, eu fui injetado');
   }
 
+  findCourseById(id: number): any{
+    return _.find(this._courses, course => _.isEqual(id, course.id));
+  }
 
-  getCourses(){
-    return [
-      {id: 1, name : 'Angular2'},
-      {id: 2, name : 'Java'},
-    ];
+  getCourses(): any[]{
+    return this._courses;
   }
 }
